da484ec74ab9220d7a52d9f36b5595224f3cbd4a
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2021-09-20 01:17:17 -0500

    correcion-error-fecth

--- src/pages/CountryForm.jsx
+++ src/pages/CountryForm.jsx
@@ -20,6 +20,7 @@ const CountryForm = () => {
         const url = `https://restcountries.eu/rest/v2/name/${nameCountry}`
         const response = await fetch(url).then(res => {setStatus(res.status); return res.json()} ).catch(er => setErr(true))
         setData(response)
+        console.log(err, ' estatus: ', status)
     }
   
     return (
@@ -36,7 +37,7 @@ const CountryForm = () => {
                 </form>
             </div>
             <div className='country-sharch'>
-                {!err || status !== 404 ? data.length > 0 ? data.map((data) => <Country key={data.name} flag={data.flag} name={data.name}></Country>) : 'cargando' : <ErrFetch alert='err de busqueda' messages='al parecer no encontramos el pais'></ErrFetch>  }
+                {!err && status !== 404 ? data.length > 0 ? data.map((data) => <Country key={data.name} flag={data.flag} name={data.name}></Country>) : 'cargando' : <ErrFetch alert='err de busqueda' messages='al parecer no encontramos el pais'></ErrFetch>  }
             </div>
         </>
     )
